2a25b79d9ab01fa9995b143a3fe02dafabc886e5
SunYunhang <suiyunshuo@gmail.com>
2022-05-30 15:42:01 +0800

    Init commit

---
+++ server.py
@@ -0,0 +1,21 @@
+import web
+
+render = web.template.render('templates/')
+
+urls = (
+    '/','index'
+)
+
+# class index:
+#     def GET(self):
+#         name = 'Bob'
+#         return render.index(name)
+
+class index:
+    def GET(self):
+        data = web.input(name=None)
+        return render.index(data.name)
+
+if __name__ == '__main__':
+    app = web.application(urls,globals())
+    app.run()
